5a762aeeefd125f679a4c52dfa5b54fd96f47d6a
henryhiraki22 <henry.hiraki@gmail.com>
2017-06-21 01:18:45 -0300

    bot.js
    for invite many contacts.

---
+++ bot.js
@@ -0,0 +1,23 @@
+var countOfPagesScrolled = 15;
+
+function scrollDown(height, countOfPagesScrolled){
+	scroll(0, document.body.clientHeight);
+	setTimeout(function(){
+		if(height != document.body.clientHeight && countOfPagesScrolled > 0){
+			scrollDown(document.body.clientHeight, --countOfPagesScrolled);
+		}else
+			return sendRequest();
+	}, 1500);
+}
+
+function sendRequest(){
+	var contactsNum = 0;
+	jQuery.each( $('button.button-secondary-small'), 	function() {
+		contactsNum++;
+		$(this).click();
+		console.log($(this).attr('title') + ' ' + contactsNum);
+	});
+	console.log('Just added contacts: ' + contactsNum);
+}
+
+scrollDown(document.body.clientHeight, countOfPagesScrolled);
